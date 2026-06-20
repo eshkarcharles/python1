4cfc73e3295a9865ab17d7daa315006987310ae8
eshkarcharles <eshkarmuchesia@gmail.com>
2026-06-20 14:27:36 +0300

    Lesson 5

---
+++ Lesson5.py
@@ -0,0 +1,8 @@
+weather = "rainy"
+if weather == "rainy":
+    print("Its a very rainy day")
+elif weather == "sunny":
+    print("Its a sunny day today")
+else:
+    print("Its a normal day today")
+    
